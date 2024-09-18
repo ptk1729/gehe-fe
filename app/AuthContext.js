a2fdc59e33b98f04b42e3e2a0bf14752fd50ed69
Prateek <prateekkukreja@outlook.com>
2024-09-18 21:35:36 +0530

    refactor data display into table

--- app/AuthContext.js
+++ app/AuthContext.js
@@ -1,40 +1,40 @@
 // context/AuthContext.js
-"use client";
+'use client'
 
-import { createContext, useState, useEffect } from "react";
-import Cookies from "js-cookie";
+import { createContext, useState, useEffect } from 'react'
+import Cookies from 'js-cookie'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 export default function AuthProvider({ children }) {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(null)
 
   useEffect(() => {
     // Check if token exists in cookies on initial load
-    const savedToken = Cookies.get("jwt");
+    const savedToken = Cookies.get('jwt')
     if (savedToken) {
-      setToken(savedToken);
+      setToken(savedToken)
     }
-  }, []);
+  }, [])
 
   const loginSaveCookie = (jwt) => {
-    const expiresDate = new Date();
-    expiresDate.setDate(expiresDate.getDate() + "8h"); // Set expiry 8 hours from now
+    const expiresDate = new Date()
+    expiresDate.setDate(expiresDate.getDate() + '8h') // Set expiry 8 hours from now
 
-    Cookies.set("jwt", token, { expires: expiresDate });
+    Cookies.set('jwt', jwt, { expires: expiresDate })
 
     // Cookies.set("jwt", jwt, { expires: 7 }); // Expires in 7 days
-    setToken(jwt);
-  };
+    setToken(jwt)
+  }
 
   const logout = () => {
-    Cookies.remove("jwt");
-    setToken(null);
-  };
+    Cookies.remove('jwt')
+    setToken(null)
+  }
 
   return (
     <AuthContext.Provider value={{ token, loginSaveCookie, logout }}>
       {children}
     </AuthContext.Provider>
-  );
+  )
 }
